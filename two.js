430680cc31baa5409eb2e76be13f605af63a4e47
Bruno Nicholas <sbnibro256@gmail.com>
2019-03-13 11:19:04 +0300

    finishing adding number two

--- two.js
+++ two.js
@@ -1,3 +1,20 @@
 /*
  * a loop which prompts for a number greater than 100,
  */
+
+ console.log('Enter a number above 100: 400');
+
+ console.log(above100(400));
+
+ function above100(num)
+ {
+ 	if (parseInt(num) >= 100) {
+ 		console.log('Correct, ' + num + ' is greater than 100');
+ 	}
+ 	else {
+ 		//
+ 		console.log('Wrong input!\nEnter a new number greater than 100!');
+ 		new_num = 105;
+ 		above100(105)
+ 	}
+ }
