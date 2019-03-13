1fd89c5e050f271a3d12f3fe344572e4e3701089
Bruno Nicholas <sbnibro256@gmail.com>
2019-03-13 10:33:01 +0300

    adding the assignment 4: number 1

---
+++ one.js
@@ -0,0 +1,12 @@
+/*
+ * This requires to print even numbers between 2 and 20
+ * Required to use a for loop
+ */
+
+ var i = 2;
+
+ for (i; i <= 20; i++) {
+ 	if (i%2 == 0) {
+ 		console.log(i);
+ 	}
+ }
---
+++ two.js
@@ -0,0 +1,3 @@
+/*
+ * a loop which prompts for a number greater than 100,
+ */
